5a97a290414d2620b0031c79505dc2baace6710e
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-16 18:28:34 +0630

    update

---
+++ CopyvsView.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+x=arr.view()
+y=arr.copy()
+print(arr)
+print(x)
+print(y)
+arr[0]=12
+print(arr)
+print(x) #the value of x[0] also changed
+print(y) #the value of y[0] did not change
+print(x.base)
+print(y.base)
