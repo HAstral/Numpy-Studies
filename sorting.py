a5519a9c44eed563c395860134f6301572f7a165
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-18 17:27:07 +0630

    update

---
+++ sorting.py
@@ -0,0 +1,10 @@
+import numpy as np
+#for 1D array
+a=np.array(['c','a','d','b'])
+print(np.sort(a))
+#for boolean array
+b=np.array([False,True,False,True])
+print(np.sort(b))
+#for 2D array
+c=np.array([[1,2,5,3],[6,2,8,4]])
+print(np.sort(c))
