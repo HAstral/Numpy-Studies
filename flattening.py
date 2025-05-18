da619806112e3341c339d038651a12c5a8f9edd3
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-18 16:56:53 +0630

    updates

--- flattening.py
+++ flattening.py
@@ -2,4 +2,4 @@ import numpy as np
 a=np.array([[1,2,3],[2,3,4],[5,6,7]])
 print(a)
 print(a.flat[6]) #it gives seventh element cause index starts from 0
-print(a.flatten())
+print(a.flatten()) #it gives a copy of the array in 1D
