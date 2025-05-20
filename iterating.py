232785cf953491ee8a12e1f69ccb0e6a9eeeb8f0
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-20 16:16:06 +0630

    update

---
+++ iterating.py
@@ -0,0 +1,15 @@
+import numpy as np
+#iterate 1D array
+arr=np.array([1,2,3,4,5,6,7])
+for x in arr:
+    print(x)
+
+#iterate 2D array
+arr2=np.array([[1,2,3],[4,5,6]])
+for x in arr2:
+    print(x)
+
+#iterate 3D array
+arr3=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+for x in arr3:
+    print(x)
