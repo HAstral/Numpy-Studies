6088e1842e4ad1492cf49dbdaf9000770f414aad
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-20 16:29:50 +0630

    updates

--- iterating.py
+++ iterating.py
@@ -9,7 +9,17 @@ arr2=np.array([[1,2,3],[4,5,6]])
 for x in arr2:
     print(x)
 
+print('scalar version of 2D array:')
+for x in arr2:
+    for y in x:        
+        print(y)
 #iterate 3D array
 arr3=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
 for x in arr3:
     print(x)
+
+print('scalar version of 3D array:')
+for x in arr3:
+    for y in x:        
+        for z in y:
+            print(z)
