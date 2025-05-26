c7a8e2806287a93b07e8d357786437c122f12e1d
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-05-26 16:37:29 +0630

    update

---
+++ arrange.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr=np.arange(0,11)
+print(arr)
+arr2=np.arange(0,11,2)
+print(arr2)
+arr3=np.linspace(0,13,8) # Generates 8 evenly spaced numbers between 0 and 13
+print(arr3)
+arr4=np.linspace(6,9,50)
+print(arr4)
